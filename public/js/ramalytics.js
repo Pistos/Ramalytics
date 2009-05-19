2f5051b51642bc8f367983f6a9f07fb95f103aea
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-05-19 19:15:16 -0400

    Added location tracking, too.

--- public/js/ramalytics.js
+++ public/js/ramalytics.js
@@ -1,3 +1,8 @@
 if( document.referrer ) {
-    document.write( '<'+'img src="http://rome.purepistos.net:8016/track?r=' + escape( document.referrer ) + '" style="float:left;" width="1" height="1"/>');
+    document.write(
+        '<'+'img src="http://rome.purepistos.net:8016/track?' +
+        'r=' + escape( document.referrer ) +
+        '&l=' + escape( document.location ) +
+        '" style="float:left;" width="1" height="1"/>'
+    );
 }
