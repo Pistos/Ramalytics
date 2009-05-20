44770d30a24711838261c04638ec4645e98a0594
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-05-20 14:23:01 -0400

    Ability to mark entire domain as seen.

--- controller/seen.rb
+++ controller/seen.rb
@@ -1,12 +1,42 @@
 class SeenController < Controller
+
   def referrer( uri_id )
-    uri = Ramalytics::URI[ uri_id ]
+    uri = Ramalytics::URI[ uri_id.to_i ]
     if uri
       uri.seen_as_referrer = true
       flash[ :success ] = 'Marked URI as seen.'
     else
       flash[ :error ] = "No URI with id #{uri_id}."
     end
+
+    redirect_referrer
+  end
+
+  def referrer_domain( uri_id )
+    r = $dbh.u(
+      %{
+        UPDATE
+          uris
+        SET
+          seen_as_referrer = true
+        FROM
+          subdomains sd
+        WHERE
+          sd.id = uris.subdomain_id
+          AND sd.domain_id = (
+            SELECT sd2.domain_id
+            FROM
+              subdomains sd2,
+              uris u2
+            WHERE
+              u2.id = ?
+              AND sd2.id = u2.subdomain_id
+            LIMIT 1
+          )
+      },
+      uri_id.to_i
+    )
+    Ramaze::Log.info r
 
     redirect_referrer
   end
