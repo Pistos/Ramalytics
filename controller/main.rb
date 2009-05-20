bbdef2601c68169f6eec8f2b35804004e3c84002
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-05-20 00:46:28 -0400

    Moved ramalytics.js into MainController. Added options.site .

--- controller/main.rb
+++ controller/main.rb
@@ -5,12 +5,27 @@ class MainController < Controller
     @title = "Welcome to Ramaze!"
   end
 
+  define_method 'ramalytics.js' do
+    js = %|
+    if( document.referrer ) {
+      document.write(
+          '<'+'img src="#{Ramalytics.options.site}/track?' +
+          'r=' + escape( document.referrer ) +
+          '&l=' + escape( document.location ) +
+          '" style="float:left;" width="1" height="1"/>'
+      );
+    }
+    |
+
+    js.gsub( / +/, ' ' ).strip
+  end
+
   def track
     location = Ramalytics::URI.parse_and_ensure_exists( request[ 'l' ] )
     referrer = Ramalytics::URI.parse_and_ensure_exists( request[ 'r' ] )
     Hit.create(
       uri_id: location.id,
-      referrer_uri_id: referrer.id
+      referrer_uri_id: referrer ? referrer.id : nil
     )
     ''
   end
--- public/js/ramalytics.js
+++
@@ -1,8 +0,0 @@
-if( document.referrer ) {
-    document.write(
-        '<'+'img src="http://rome.purepistos.net:8016/track?' +
-        'r=' + escape( document.referrer ) +
-        '&l=' + escape( document.location ) +
-        '" style="float:left;" width="1" height="1"/>'
-    );
-}
