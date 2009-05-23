7ae0a9f120bc64784733a19f6be51cd019c3b8b4
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-05-23 16:09:51 -0400

    Added a site picker to the nav bar.

--- public/js/main.js
+++ public/js/main.js
@@ -2,4 +2,13 @@ $( document ).ready( function() {
     $( 'textarea' ).focus( function() {
         this.select();
     } );
+
+    $( '#site-picker' ).change( function() {
+        window.location = '/stats/' + $('option:selected', this).eq(0).val();
+    } );
+
+    var a = /\/stats\/(\d+)/( window.location );
+    if( a ) {
+        $( '#site-picker' ).val( a[ 1 ] );
+    }
 } );
