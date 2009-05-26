a25e68f8a789c1bc950d68cdcade321af5ed57f4
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-05-26 00:52:44 -0400

    Mark domain as seen and unseen.

--- controller/seen.rb
+++ controller/seen.rb
@@ -36,40 +36,64 @@ class SeenController < Controller
     end
   end
 
-  def referrer_domain( uri_id )
+  def referrer_domain( uri_id, action = 'seen' )
     uri = Ramalytics::URI[ uri_id.to_i ]
     if uri
-      num_inserted = $dbh.i(
-        %{
-          INSERT INTO referrer_sightings (
-            uri_id,
-            user_id
-          ) SELECT
-            u.id,
-            ?
-          FROM
-              uris u
-            , subdomain_paths sp
-          WHERE
-            sp.subdomain_id = ?
-            AND u.subdomain_path_id = sp.id
-            AND NOT EXISTS(
-              SELECT 1
-              FROM referrer_sightings rs
-              WHERE
-                rs.uri_id = u.id
-                AND rs.user_id = ?
-              LIMIT 1
-            )
-        },
-        user.id,
-        uri.subdomain.id,
-        user.id
-      )
-      if num_inserted > 0
-        flash[ :success ] = "Marked #{num_inserted} referring URIs as seen."
-      else
-        flash[ :error ] = "No referring URIs marked as seen."
+      if action == 'seen'
+        num_inserted = $dbh.i(
+          %{
+            INSERT INTO referrer_sightings (
+              uri_id,
+              user_id
+            ) SELECT
+              u.id,
+              ?
+            FROM
+                uris u
+              , subdomain_paths sp
+            WHERE
+              sp.subdomain_id = ?
+              AND u.subdomain_path_id = sp.id
+              AND NOT EXISTS(
+                SELECT 1
+                FROM referrer_sightings rs
+                WHERE
+                  rs.uri_id = u.id
+                  AND rs.user_id = ?
+                LIMIT 1
+              )
+          },
+          user.id,
+          uri.subdomain.id,
+          user.id
+        )
+        if num_inserted > 0
+          flash[ :success ] = "Marked #{num_inserted} referring URIs as seen."
+        else
+          flash[ :error ] = "No referring URIs marked as seen."
+        end
+      elsif action == 'unseen'
+        num_deleted = $dbh.d(
+          %{
+            DELETE FROM
+              referrer_sightings
+            USING
+                uris u
+              , subdomain_paths sp
+            WHERE
+              sp.subdomain_id = ?
+              AND u.subdomain_path_id = sp.id
+              AND referrer_sightings.uri_id = u.id
+              AND referrer_sightings.user_id = ?
+          },
+          uri.subdomain.id,
+          user.id
+        )
+        if num_deleted > 0
+          flash[ :success ] = "Marked #{num_deleted} URIs as unseen."
+        else
+          flash[ :error ] = 'Failed to mark URI as unseen.'
+        end
       end
     end
 
